98944fd013a21ad936152562d670d07625ecbc31
Vincent Sourice <v.sourice@purjus.fr>
2019-05-31 13:17:42 +0200

    last v mithril

--- server/server-dev.js
+++ server/server-dev.js
@@ -1,4 +1,3 @@
-//import 'regenerator-runtime/runtime'; //https://github.com/babel/babel-loader/issues/484
 import Koa from 'koa';
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
@@ -7,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 require('mithril/test-utils/browserMock')(global);
 
 var m = require('mithril');
-var render = require('mithril-node-render')
+var render = require('mithril-node-render');
 
 app.use(async (ctx) => {
     ctx.body = await render(m('span', 'Voici un exemple de SSR'));
