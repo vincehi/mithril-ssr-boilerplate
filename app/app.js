7c5d7f18a66b2311631c0410660541e440f499b1
Vincent Sourice <v.sourice@purjus.fr>
2019-06-03 12:17:51 +0200

    before integre modif general

--- app/app.js
+++ app/app.js
@@ -6,13 +6,15 @@ const clientRoutes = {};
 Object.keys(routes).forEach((route) => {
     clientRoutes[route] = {
         onmatch: (args, requestedPath) => {
-            routes[route].onmatch ? routes[route].onmatch(attrs, requestedPath) : routes[route]
+            return routes[route].onmatch ? routes[route].onmatch(attrs, requestedPath) : routes[route]
         },
-        render: vnode => {
-            vnode
+        render: (vnode) => {
+            return vnode
         }
     };
 });
 
 m.route.prefix('');
 m.route(document.getElementById('mainContent'), '/', clientRoutes);
+
+console.log("Je suis bien chargé !")
