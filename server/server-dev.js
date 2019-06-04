b5695a1e36dc7a8821996dfba5f046b02e5d2d5e
Vincent Sourice <v.sourice@purjus.fr>
2019-06-04 21:30:46 +0200

    rename statics

--- server/server-dev.js
+++ server/server-dev.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const Koa = require('koa');
-const server = require('koa-static');
+const statics = require('koa-static');
 const router = require('koa-route');
 const app = new Koa();
 
@@ -8,20 +8,34 @@ const PORT = process.env.PORT || 3000;
 
 // use a mock DOM so we can run mithril on the server
 require('mithril/test-utils/browserMock')(global);
+global.window.XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
 
 var toHTML = require('mithril-node-render');
+const m = require('mithril');
 
 const routes = require('../app/common/routes');
 
 Object.keys(routes).forEach((route) => {
     app.use(router.get(route, async (ctx, params) => {
 
+        // @todo : start test
+        // m.request({
+        //     method: "GET",
+        //     url: "https://randomuser.me/api/",
+        //     background: true // Permet de ne pas redessiner le composant apres réponse
+        // })
+        //     .then(resp => console.log(resp))
+        //     .catch((err) => {
+        //         console.log(err);
+        //     });
+        // end : test
+
         ctx.body = await toHTML(routes[route])
 
     }));
 });
 
-app.use(server(path.join(__dirname, "..", "build/assets")));
+app.use(statics(path.join(__dirname, "..", "build/assets")));
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
