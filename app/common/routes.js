02f38f05cc54dc1db225ecfe5904fcf443b32416
Vincent Sourice <v.sourice@purjus.fr>
2019-06-12 23:13:00 +0200

    clean

--- app/common/routes.js
+++ app/common/routes.js
@@ -1,13 +1,15 @@
-const m = require('mithril');
-
 module.exports = {
     '/': {
         name: 'Accueil',
-        component: () => new Promise(function(resolve) {require.ensure([], () => resolve(require('../pages/Home.js')), /* chunkFilename */ 'home')})
+        component: () => new Promise(function(resolve) {
+            require.ensure([], () => resolve(require('../pages/Home.js')), /* chunkFilename */ 'home')
+        })
         // component: () => new Promise(function(resolve) {require(['../pages/Home.js'], resolve)}) // comme avant
     },
     '/contact': {
         name: 'Contact',
-        component: () => new Promise(function(resolve) {require.ensure([], () => resolve(require('../pages/Contact.js')), /* chunkFilename */ 'contact')})
+        component: () => new Promise(function(resolve) {
+            require.ensure([], () => resolve(require('../pages/Contact.js')), /* chunkFilename */ 'contact')
+        })
     }
 };
--- server/server-dev.js
+++ server/server-dev.js
@@ -2,19 +2,15 @@ const path = require('path');
 const Koa = require('koa');
 const statics = require('koa-static');
 const router = require('koa-route');
-const app = new Koa();
-
-const PORT = process.env.PORT || 3000;
-
-// use a mock DOM so we can run mithril on the server
-require('mithril/test-utils/browserMock')(global);
+require('mithril/test-utils/browserMock')(global); // use a mock DOM so we can run mithril on the server
 global.window.XMLHttpRequest = require('w3c-xmlhttprequest').XMLHttpRequest;
-
 var toHTML = require('mithril-node-render');
 const m = require('mithril');
-
 const routes = require('app/common/routes');
 const Layout = require('app/components/Layout');
+
+const app = new Koa();
+const PORT = process.env.PORT || 3000;
 
 
 Object.keys(routes).forEach((route) => {
