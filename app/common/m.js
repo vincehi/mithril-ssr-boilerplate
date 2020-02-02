f5d1a683c6fe6530ac702d735caa348efb8b180d
souricevincent <souricevincent@icloud.com>
2020-02-02 13:07:45 +0100

    update mithril-node-render and server require mithril mithril/api/router

--- app/common/m.js
+++ app/common/m.js
@@ -2,8 +2,6 @@ if (process.browser) {
     module.exports = require('mithril');
 } else {
     module.exports = require('mithril/hyperscript');
-    module.exports.route = {
-        Link: "a" // Fix 'Link' of undefined
-    };
+    module.exports.route = require('mithril/api/router')(window, null);
     module.exports.request = require('mithril/request/request')(window, require("mithril/promise/promise"), null).request;
 }
