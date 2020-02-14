eaecec0187f6b16fd018baac5e1d5c4fa51439e3
souricevincent <souricevincent@icloud.com>
2020-02-15 00:04:40 +0100

    jsx for layout

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -10,39 +10,41 @@ const Script = {
   }
 };
 
+const mainContent = (vnode) => (
+  <>
+    <Header/>
+    <vnode.attrs.module.tag stateman={vnode.attrs.module.stateman} />
+    <Footer/>
+  </>
+);
+
 const LayoutClient = {
-  view: (vnode) => {
-    return [
-      <Header/>,
-      m(vnode.attrs.module.tag, { stateman: vnode.attrs.module.stateman }),
-      <Footer/>
-    ];
-  }
+  view: (vnode) => mainContent(vnode)
 };
 
 const LayoutServer = {
   view: (vnode) => {
-    return [
-      m('!doctype[html]'),
-      m('html', { lang: 'fr' }, [
-        m('head', [
-          <meta charset="utf-8"/>,
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>,
-          <meta http-equiv="x-ua-compatible" content="ie=edge"/>,
-          <meta name="description" content="description ici"/>,
-          <title>{vnode.attrs.module.tag.data.title}</title>
-        ]),
-        m('body', [
-          <div id="mainContent">
-            <Header/>
-            {m(vnode.attrs.module.tag, { stateman: vnode.attrs.module.stateman })}
-            <Footer/>
-          </div>,
-          <Script stateman={vnode.attrs.module.stateman}/>,
-          <script src="/js/app.js"/>
-        ])
-      ])
-    ];
+    return (
+      <>
+        {m('!doctype[html]')}
+        <html lang="fr">
+          <head>
+            <meta charSet="utf-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+            <meta http-equiv="x-ua-compatible" content="ie=edge"/>
+            <meta name="description" content="description ici"/>
+            <title>{vnode.attrs.module.tag.data.title}</title>
+          </head>
+          <body>
+            <div id="mainContent">
+              {mainContent(vnode)}
+            </div>
+            <Script stateman={vnode.attrs.module.stateman}/>
+            <script src="/js/app.js"/>
+          </body>
+        </html>
+      </>
+    );
   }
 };
 
--- app/pages/Home.js
+++ app/pages/Home.js
@@ -4,7 +4,7 @@ const Home = {
   data: {
     title: 'Home'
   },
-  view: () => {
+  view: (vnode) => {
     return (
       <div>
         Contenu de la page Homepage !
