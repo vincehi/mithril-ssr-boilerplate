a326d4cc25b8f3c8ae5c71e0ae8ff074fef9dab9
SOURICE Vincent <v.sourice@netfective.com>
2020-03-03 13:42:15 +0100

    require in m.js

--- app/common/m.js
+++ app/common/m.js
@@ -1,14 +1,15 @@
 /*
 Mithril remove m.jsonp and m.request
  */
-import hyperscript from 'mithril/hyperscript';
-import router from 'mithril/api/router';
+
+const hyperscript = require('mithril/hyperscript');
+const router = require('mithril/api/router');
 
 let mithril = {};
 
 if (process.env.BROWSER_ENV) {
-  const renderBrowser = import('mithril/render/render').then(({ default: resp }) => resp(window));
-  const mountRedrawBrowser = import('mithril/api/mount-redraw').then(({ default: resp }) => resp(renderBrowser, requestAnimationFrame, console));
+  const renderBrowser = require('mithril/render/render')(window);
+  const mountRedrawBrowser = require('mithril/api/mount-redraw')(renderBrowser, requestAnimationFrame, console);
   mithril = function m() {
     return hyperscript.apply(this, arguments);
   };
@@ -19,12 +20,12 @@ if (process.env.BROWSER_ENV) {
   mithril.route = router(window, mountRedrawBrowser);
   mithril.render = renderBrowser;
   mithril.redraw = mountRedrawBrowser.redraw;
-  mithril.parseQueryString = import('mithril/querystring/parse').then(({ default: resp }) => resp);
-  mithril.buildQueryString = import('mithril/querystring/build').then(({ default: resp }) => resp);
-  mithril.parsePathname = import('mithril/pathname/parse').then(({ default: resp }) => resp);
-  mithril.buildPathname = import('mithril/pathname/build').then(({ default: resp }) => resp);
-  mithril.vnode = import('mithril/render/vnode').then(({ default: resp }) => resp);
-  mithril.PromisePolyfill = import('mithril/promise/polyfill').then(({ default: resp }) => resp);
+  mithril.parseQueryString = require('mithril/querystring/parse');
+  mithril.buildQueryString = require('mithril/querystring/build');
+  mithril.parsePathname = require('mithril/pathname/parse');
+  mithril.buildPathname = require('mithril/pathname/build');
+  mithril.vnode = require('mithril/render/vnode');
+  mithril.PromisePolyfill = require('mithril/promise/polyfill');
 } else {
   mithril = hyperscript;
   mithril.route = router(undefined, undefined);
